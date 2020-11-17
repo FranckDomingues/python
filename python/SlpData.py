7a1ed0c181fb37a766917c56bcef5512309b2e45
FranckDomingues <franciscomicros1@gmail.com>
2020-11-17 09:54:31 -0500

    17-Nov-2020

--- python/SlpData.py
+++ python/SlpData.py
@@ -91,8 +91,11 @@ sleepdf.to_csv('./sleep'+today+'.csv', columns = ['Time','State','Interpreted'],
 sleep_data_to_send=fitbit_stats3['summary']
 sleep_data_to_send['date']=fitbit_stats3['sleep'][0]['startTime']
 
-hearts_data_to_send=dict()
-hearts_data_to_send['heartZones']=hearts['activities-heart'][0]['value']['heartRateZones']
-hearts_data_to_send['date']=hearts['activities-heart'][0]['dateTime']
+hearts_data_to_send=dict() #send data information
+hearts_data_to_send['heartZones']=stats2['activities-heart'][0]['value']['heartRateZones']
+hearts_data_to_send['date']=stats2['activities-heart'][0]['dateTime']
 
-r = req.post('http://localhost:8080/heart',json=hearts_data_to_send)
+print(stats2)
+print(hearts_data_to_send)
+
+r = req.post('http://localhost:8081/heart',json=hearts_data_to_send)
